8bdb639745e2ba318f1654533171c3f7d500cdf8
Heather <50023445+heatherjbell@users.noreply.github.com>
2019-06-20 23:48:22 -0500

    Final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,53 @@
-print("hi")
-print("new")
+
+import csv
+
+candidates = []
+num_votes = 0
+vote_counts = []
+file_name = 'election_data.csv'
+filepath = ('raw_data',file_name)
+with open('election_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
+    line = next(csvreader,None)
+    for line in csvreader:
+        num_votes = num_votes + 1
+        candidate = line[2]
+        if candidate in candidates:
+            candidate_index = candidates.index(candidate)
+            vote_counts[candidate_index] = vote_counts[candidate_index] + 1
+        else:
+            candidates.append(candidate)
+            vote_counts.append(1)
+percentages = []
+max_votes = vote_counts[0]
+max_index = 0
+for count in range(len(candidates)):
+    vote_percentage = vote_counts[count]/num_votes*100
+    percentages.append(vote_percentage)
+    if vote_counts[count] > max_votes:
+        max_votes = vote_counts[count]
+        print(max_votes)
+        max_index = count
+winner = candidates[max_index]
+
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {num_votes}")
+for count in range(len(candidates)):
+    print(f"{candidates[count]}: {percentages[count]}% ({vote_counts[count]})")
+print("---------------------------")
+print(f"Winner: {winner}")
+print("---------------------------")
+
+write_file = f"pypoll.txt"
+filewriter = open(write_file, mode = 'w')
+
+filewriter.write("Election Results\n")
+filewriter.write("--------------------------\n")
+filewriter.write(f"Total Votes: {num_votes}\n")
+for count in range(len(candidates)):
+    filewriter.write(f"{candidates[count]}: {percentages[count]}% ({vote_counts[count]})\n")
+filewriter.write("---------------------------\n")
+filewriter.write(f"Winner: {winner}\n")
+filewriter.write("---------------------------\n")
+
